e9b4c8d5b7cb42c7f338c45339a64ea06a2a42e7
Amaël <amael.rouleau@etu.univ-nantes.fr>
2026-01-17 22:13:40 +0100

    feat: add mod float in utility + some more test

--- src/utility.rs
+++ src/utility.rs
@@ -1,11 +1,13 @@
 pub mod string {
 
     pub fn expand_or_truncate(mut string: String, max_width: usize) -> String {
-        if string.len() > max_width {
-            string.truncate(max_width);
+        if string.chars().count() > max_width {
+            string.truncate(max_width-4);
+            string.push_str("...");
+            string = format!("{}{}", string, " ".repeat(max_width-string.chars().count()));
         }
         else {
-            string = format!("{}{}", string, " ".repeat(max_width - string.len()));
+            string = format!("{}{}", string, " ".repeat(max_width - string.chars().count()));
         }
         string
     }
@@ -18,15 +20,39 @@ pub mod string {
 
 }
 
+pub mod float {
+    pub fn truncate(float: f32, precision: usize) -> f32 {
+        if precision == 0 {
+            return float.trunc();
+        }
+        let factor = 10.0_f32.powi(precision as i32);
+        (float * factor).trunc() / factor
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::string::{expand_or_truncate, center};
+    use super::float::truncate;
     #[test]
     fn test_expand_or_truncate() {
         assert_eq!(expand_or_truncate("Hello".to_string(), 10), "Hello     ".to_string());
+        assert_eq!(expand_or_truncate("Hello World".to_string(), 10), "Hello ... ".to_string());
+        assert_eq!(expand_or_truncate("Hello".to_string(), 5), "Hello".to_string());
     }
     #[test]
     fn test_center() {
         assert_eq!(center("Hello".to_string(), 10), "  Hello  ".to_string());
+        assert_eq!(center("Hello World".to_string(), 15), "  Hello World  ".to_string());
+        assert_eq!(center("Hello".to_string(), 6), "Hello".to_string());
+        assert_eq!(center("Hello ".to_string(), 6), "Hello ".to_string());
+        assert_eq!(center(" Hello".to_string(), 6), " Hello".to_string());
+        assert_eq!(center("Hello ".to_string(), 8), " Hello  ".to_string());
+    }
+    #[test]
+    fn test_truncate() {
+        assert_eq!(truncate(1.23456789, 2), 1.23);
+        assert_eq!(truncate(1.23456789, 0), 1.0);
+        assert_eq!(truncate(1.23456789, 3), 1.234);
     }
 }
