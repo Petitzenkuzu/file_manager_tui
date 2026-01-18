6734f649eccde55e16ced45b17251c154969e421
Amaël <amael.rouleau@etu.univ-nantes.fr>
2026-01-18 17:53:25 +0100

    feat: reload feature

--- src/file_manager.rs
+++ src/file_manager.rs
@@ -6,7 +6,7 @@ pub struct FileManager {
     pub path: PathBuf,
     pub files: Vec<File>,
 }
-
+// public methods
 impl FileManager {
     pub fn new(path :PathBuf) -> std::io::Result<Self> {
         let files = fs::read_dir(&path)?.filter_map(|entry| File::try_from(entry.ok()?).ok()).collect::<Vec<File>>();
@@ -21,27 +21,38 @@ impl FileManager {
         &self.files
     }
 
-    pub fn go_to_parent(&mut self) -> std::io::Result<()> {
+    pub fn dispatch(&mut self, action: Action) -> std::io::Result<()> {
+        match action {
+            Action::Open(index) => self.open(index),
+            Action::GoToParent => self.go_to_parent(),
+            Action::Reload => self.reload_files(),
+        }
+    }
+}
+
+// private action methods
+impl FileManager {
+    fn go_to_parent(&mut self) -> std::io::Result<()> {
         self.path = match self.path.parent() {
             Some(parent) => parent.to_path_buf(),
             None => self.path.clone()
         };
-        self.files = fs::read_dir(&self.path)?.filter_map(|entry| File::try_from(entry.ok()?).ok()).collect::<Vec<File>>();
+        self.reload_files()?;
         Ok(())
     }
 
-    pub fn open_folder(&mut self, file: File) -> std::io::Result<()> {
+    fn open_folder(&mut self, file: File) -> std::io::Result<()> {
         self.path.push(file.name.clone());
-        self.files = fs::read_dir(&self.path)?.filter_map(|entry| File::try_from(entry.ok()?).ok()).collect::<Vec<File>>();
+        self.reload_files()?;
         Ok(())
     }
 
-    pub fn open_file(&mut self, file: File) -> std::io::Result<()> {
-        std::process::Command::new("cmd").arg("/c").arg("start").arg("cmd").arg("echo").arg(file.name.clone()).spawn()?;
+    fn open_file(&mut self, file: File) -> std::io::Result<()> {
+        std::process::Command::new("cmd").arg("/c").arg("start").arg(self.path().join(&file.name).to_string_lossy().to_string()).spawn()?;
         Ok(())
     }
 
-    pub fn open_link(&mut self, file: File) -> std::io::Result<()> {
+    fn open_link(&mut self, file: File) -> std::io::Result<()> {
         match &file.file_type {
             FileType::Link { target, is_dead } => {
                 if *is_dead {
@@ -51,12 +62,12 @@ impl FileManager {
                 let metadata = fs::metadata(&path)?;
                 if metadata.is_dir() {
                     self.path = path;
-                    self.files = fs::read_dir(&self.path)?.filter_map(|entry| File::try_from(entry.ok()?).ok()).collect::<Vec<File>>();
+                    self.reload_files()?;
                 } else {
                     // in a first time, we go to the parent folder of the pointed file, later i'll fix this to open the file with the correct thing
                     path.pop();
                     self.path = path;
-                    self.files = fs::read_dir(&self.path)?.filter_map(|entry| File::try_from(entry.ok()?).ok()).collect::<Vec<File>>();
+                    self.reload_files()?;
 
                 }
             },
@@ -65,7 +76,7 @@ impl FileManager {
         Ok(())
     }
 
-    pub fn open(&mut self, index: usize) -> std::io::Result<()> {
+    fn open(&mut self, index: usize) -> std::io::Result<()> {
         match &self.files[index].file_type {
             FileType::Folder => self.open_folder(self.files[index].clone())?,
             FileType::File => self.open_file(self.files[index].clone())?,
@@ -74,17 +85,17 @@ impl FileManager {
         }
         Ok(())
     }
-
-    pub fn dispatch(&mut self, action: Action) -> std::io::Result<()> {
-        match action {
-            Action::Open(index) => self.open(index),
-            Action::GoToParent => self.go_to_parent(),
-        }
-    }
-
 }
 
+// private utility methods
+impl FileManager {
+    fn reload_files(&mut self) -> std::io::Result<()> {
+        self.files = fs::read_dir(&self.path)?.filter_map(|entry| File::try_from(entry.ok()?).ok()).collect::<Vec<File>>();
+        Ok(())
+    }
+}
 pub enum Action {
     Open(usize),
     GoToParent,
+    Reload,
 }
