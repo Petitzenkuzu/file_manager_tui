9be76d6038ed628704d9ba77c2a7800dfdea2d77
Amaël <amael.rouleau@etu.univ-nantes.fr>
2026-01-24 19:44:36 +0100

    feat!: the app now use a worker thread

--- src/file_manager/file_manager.rs
+++ src/file_manager/file_manager.rs
@@ -1,19 +1,23 @@
 use std::path::PathBuf;
 use std::fs;
-use crate::file_manager::FileStorage;
-use crate::file_manager::{StorageAction, StorageResponse, StorageError};
 use thiserror::Error;
 use crate::file_manager::{File, FileType};
+use std::sync::mpsc;
+use crate::workers::LightWorkerMessage;
+use crate::workers::LightWorkerAction;
+use std::sync::mpsc::SendError;
+use crate::workers::LightWorkerResponse;
 
 pub struct FileManager {
     path: PathBuf,
-    file_storage: FileStorage,
+    files: Vec<File>,
+    light_sync_id: usize,
+    light_worker_channel: mpsc::Sender<LightWorkerMessage>,
 }
 // public methods
 impl FileManager {
-    pub fn new(path :&PathBuf) -> std::io::Result<Self> {
-        let file_storage = FileStorage::new(path)?;
-        Ok(Self { path: path.clone(), file_storage })
+    pub fn new(path :&PathBuf, light_sync_id: usize, light_worker_channel: mpsc::Sender<LightWorkerMessage>) -> Self {
+        Self { path: path.clone(), files: Vec::new(), light_sync_id, light_worker_channel }
     }
 
     pub fn path(&self) -> &PathBuf {
@@ -21,10 +25,26 @@ impl FileManager {
     }
 
     pub fn files(&self) -> &Vec<File> {
-        self.file_storage.files()
+        &self.files
     }
 
-    pub fn dispatch(&mut self, action: FileManagerAction) -> Result<FileManagerResponse, FileManagerError> {
+    pub fn light_sync_id(&self) -> usize {
+        self.light_sync_id
+    }
+
+    pub fn increment_light_sync_id(&mut self) {
+        self.light_sync_id = (self.light_sync_id + 1) % usize::MAX;
+    }
+
+    pub fn set_light_worker_channel(&mut self, channel: mpsc::Sender<LightWorkerMessage>) {
+        self.light_worker_channel = channel;
+    }
+
+    pub fn shutdown(&self) {
+        let _ = self.light_worker_channel.send(LightWorkerMessage::Shutdown);
+    }
+
+    pub fn dispatch(&mut self, action: FileManagerAction) -> Result<(), FileManagerError> {
         match action {
             FileManagerAction::Open(index) => self.open(index),
             FileManagerAction::GoToParent => self.go_to_parent(),
@@ -34,92 +54,72 @@ impl FileManager {
             },
         }
     }
+
+    pub fn consume_response(&mut self, response: LightWorkerResponse){
+        match response {
+            LightWorkerResponse::Loaded(files, path) => {
+                self.path = path;
+                self.files = files;
+                self.increment_light_sync_id();
+            },
+        }
+    }
 }
 
 // private action methods
 impl FileManager {
-    fn go_to_parent(&mut self) -> Result<FileManagerResponse, FileManagerError> {
+    fn go_to_parent(&mut self) -> Result<(), FileManagerError> {
         let parent = match self.path.parent() {
             Some(parent) => parent.to_path_buf(),
             None => PathBuf::from("/")
         };
-        match self.file_storage.dispatch(StorageAction::Load(&parent)) {
-            Ok(response) => {
-                match response {
-                    StorageResponse::Loaded => {
-                        self.path = parent;
-                        Ok(FileManagerResponse::WentToParent)
-                    },
-                    //_ => Err(FileManagerError::GoToParentFailed("Failed to load parent directory".to_string())),
-                }
-            },
-            Err(error) => Err(FileManagerError::GoToParentFailed(error)),
-        }
+        self.light_worker_channel.send(LightWorkerMessage::WorkerAction{sync_id: self.light_sync_id, action: LightWorkerAction::Load(parent)})?;
+        Ok(())
     }
 
-    fn open_folder(&mut self, file: File) -> Result<FileManagerResponse, FileManagerError> {
-        let path = self.path.join(file.name());
-        match self.file_storage.dispatch(StorageAction::Load(&path)) {
-            Ok(response) => {
-                match response {
-                    StorageResponse::Loaded => {
-                        self.path = path;
-                        Ok(FileManagerResponse::Opened)
-                    },
-                }
-            },
-            Err(error) => Err(FileManagerError::OpenFailed(error.to_string())),
-        }
+    fn open_folder(&self, path: PathBuf) -> Result<(), FileManagerError> {
+        self.light_worker_channel.send(LightWorkerMessage::WorkerAction{sync_id: self.light_sync_id, action: LightWorkerAction::Load(path)})?;
+        Ok(())
     }
 
-    fn open_file(&mut self, file: File) -> Result<FileManagerResponse, FileManagerError> {
-        std::process::Command::new("cmd").arg("/c").arg("start").arg(self.path().join(file.name()).to_string_lossy().to_string()).spawn().map_err(|_e| FileManagerError::OpenFailed("Failed to open file".to_string()))?;
-        Ok(FileManagerResponse::Opened)
+    fn open_file(&self, path: PathBuf) -> Result<(), FileManagerError> {
+        std::process::Command::new("cmd").arg("/c").arg("start").arg(path.to_string_lossy().to_string()).spawn().map_err(|_e| FileManagerError::OpenFileFailed("Failed to open file".to_string()))?;
+        Ok(())
     }
 
-    fn open_link(&mut self, file: File) -> Result<FileManagerResponse, FileManagerError> {
+    fn open_link(&self, file: &File) -> Result<(), FileManagerError> {
         match file.file_type() {
             FileType::Link { target, is_dead } => {
                 if *is_dead {
-                    return Err(FileManagerError::OpenFailed("Dead Symbolic Link".to_string()));
+                    return Err(FileManagerError::OpenFileFailed("Dead Symbolic Link".to_string()));
                 }
-                let mut path = fs::canonicalize(target).map_err(|e| FileManagerError::OpenFailed("Failed to resolve the link".to_string()))?;
+                let path = fs::canonicalize(target).map_err(|e| FileManagerError::OpenFileFailed("Failed to resolve the link".to_string()))?;
 
-                let metadata = fs::metadata(&path).map_err(|_e| FileManagerError::OpenFailed("Failed to resolve targeted file".to_string()))?;
+                let metadata = fs::metadata(&path).map_err(|_e| FileManagerError::OpenFileFailed("Failed to resolve targeted file".to_string()))?;
+
                 if metadata.is_dir() {
-                    self.file_storage.dispatch(StorageAction::Load(&path))?;
-                    self.path = path;
+                    self.open_folder(path)?;
                 } else {
-                    // in a first time, we go to the parent folder of the pointed file, later i'll fix this to open the file with the correct thing
-                    path.pop();
-                    self.file_storage.dispatch(StorageAction::Load(&path))?;
-                    self.path = path;
-
+                    self.open_file(path)?;
                 }
             },
-            _ => return Err(FileManagerError::OpenFailed("Invalid file type".to_string())),
+            _ => return Err(FileManagerError::OpenUnknownFileType("Invalid file type".to_string())),
         }
-        Ok(FileManagerResponse::Opened)
+        Ok(())
     }
 
-    fn open(&mut self, index: usize) -> Result<FileManagerResponse, FileManagerError> {
+    fn open(&self, index: usize) -> Result<(), FileManagerError> {
         match self.files()[index].file_type() {
-            FileType::Folder => self.open_folder(self.files()[index].clone()),
-            FileType::File => self.open_file(self.files()[index].clone()),
-            FileType::Link { .. } => self.open_link(self.files()[index].clone()),
-            FileType::Unknown => return Err(FileManagerError::OpenFailed("Unknown file type".to_string())),
+            FileType::Folder => self.open_folder(self.path.join(self.files[index].name())),
+            FileType::File => self.open_file(self.path.join(self.files[index].name())),
+            FileType::Link { .. } => self.open_link(&self.files[index]),
+            FileType::Unknown => return Err(FileManagerError::OpenUnknownFileType("Unknown file type".to_string())),
         }
     }
 
-    fn reload_files(&mut self) -> Result<FileManagerResponse, FileManagerError> {
-        match self.file_storage.dispatch(StorageAction::Load(&self.path)) {
-            Ok(response) => {
-                match response {
-                    StorageResponse::Loaded => Ok(FileManagerResponse::Reloaded),
-                }
-            },
-            Err(error) => Err(FileManagerError::ReloadFailed(error.to_string())),
-        }
+    fn reload_files(&self) -> Result<(), FileManagerError> {
+        self.light_worker_channel.send(LightWorkerMessage::WorkerAction{sync_id: self.light_sync_id, action: LightWorkerAction::Load(self.path.clone())})?;
+        Ok(())
     }
 
     fn _create_folder(&mut self, path: PathBuf) -> std::io::Result<()> {
@@ -144,20 +144,16 @@ pub enum FileManagerResponse {
 
 #[derive(Error, Debug)]
 pub enum FileManagerError {
+    #[error("Error sending message: {0}")]
+    SendMessageFailed(String),
     #[error("Error opening file: {0}")]
-    OpenFailed(String),
-    #[error("Error going to parent directory: {0}")]
-    GoToParentFailed(StorageError),
-    #[error("Error creating folder: {0}")]
-    CreateFolderFailed(String),
-    #[error("Error reloading files: {0}")]
-    ReloadFailed(String),
+    OpenFileFailed(String),
+    #[error("Error opening unknown file type: {0}")]
+    OpenUnknownFileType(String),
 }
 
-impl From<crate::file_manager::StorageError> for FileManagerError {
-    fn from(error: crate::file_manager::StorageError) -> Self {
-        match error {
-            crate::file_manager::StorageError::LoadFailed(message) => FileManagerError::OpenFailed(message),
-        }
+impl From<SendError<LightWorkerMessage>> for FileManagerError {
+    fn from(error: SendError<LightWorkerMessage>) -> Self {
+        FileManagerError::SendMessageFailed(error.to_string())
     }
 }
--- src/file_manager/mod.rs
+++ src/file_manager/mod.rs
@@ -7,9 +7,3 @@ pub use file_manager::FileManager;
 pub use file_manager::FileManagerAction;
 pub use file_manager::FileManagerResponse;
 pub use file_manager::FileManagerError;
-
-pub mod file_storage;
-pub use file_storage::FileStorage;
-pub use file_storage::StorageAction;
-pub use file_storage::StorageResponse;
-pub use file_storage::StorageError;
